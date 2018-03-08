0e8eb6d20c45459ee23681ff462562362fa39864
Mikołaj Walków <mikolaj.walkow@gmail.com>
2018-03-08 23:24:26 +0100

    address rest controller added

---
+++ src/main/java/pl/mwa/address/AddressResource.java
@@ -0,0 +1,78 @@
+package pl.mwa.address;
+
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import pl.mwa.client.Client;
+
+
+
+
+@RestController
+@RequestMapping("/addresses")
+public class AddressResource {
+
+	@Autowired
+	AddressService service;
+	
+	
+	private static final Logger log = LoggerFactory.getLogger(AddressResource.class);
+	
+	@GetMapping("/{id}")
+	ResponseEntity getAddress(@PathVariable("id") Long id ) {
+		return ResponseEntity.ok(service.getAddress(id));
+	}
+
+	@GetMapping
+	ResponseEntity getAddresses() {
+		return ResponseEntity.ok(service.getAddresses());
+	}
+	
+	@GetMapping("/search")
+	ResponseEntity getSearchRequestAddresses(@RequestParam(name="street", required=false) String street, 
+			@RequestParam(name="streetNumber", required=false) String streetNumber,
+			@RequestParam(name="additionalNumber", required=false) String additionalNumber,
+			@RequestParam(name="postalCode", required=false) String postalCode, 
+			@RequestParam(name="city", required=false) String city, 
+			@RequestParam(name="client", required=false) Client client) {
+		return ResponseEntity.ok(service.getAddressesSearch(street, 
+				streetNumber, additionalNumber, postalCode, city, client));
+	}
+	
+	@PostMapping
+	ResponseEntity createAddress(@RequestBody @Valid CreateAddressDto dto) {
+		long id = service.createAddress(dto);
+		log.info("Address with id: "+id+" created");
+		return ResponseEntity.ok(id);
+	}
+	
+	@PutMapping
+	ResponseEntity updateAddress(@RequestBody @Valid AddressDto dto) {
+		service.update(dto);
+		log.info("Address with id: "+dto.getId()+" updated");
+		return ResponseEntity.accepted().build();
+	}
+	
+	@DeleteMapping("/{id}")
+	ResponseEntity deleteAddress(@PathVariable("id") long id) {
+		service.deleteAddress(id);
+		log.info("Address with id: "+id+" deleted");
+		return ResponseEntity.accepted().build();
+	}
+	
+	
+	
+}
--- src/main/java/pl/mwa/address/AddressService.java
+++ src/main/java/pl/mwa/address/AddressService.java
@@ -1,5 +1,6 @@
 package pl.mwa.address;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -7,6 +8,7 @@ import java.util.stream.StreamSupport;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import pl.mwa.client.Client;
@@ -17,74 +19,68 @@ import pl.mwa.util.EntityUtils;
 public class AddressService {
 
 	private static final Logger log = LoggerFactory.getLogger(AddressService.class);
-	
+
+	@Autowired
 	AddressRepository repository;
-	
 
 	Long createAddress(CreateAddressDto dto) {
 		Address address = repository.save(AddressMapper.toEntity(dto));
 		return address.getId();
 	}
-	
+
 	AddressDto getAddress(long id) {
-		return repository.findById(id)
-				.map(AddressMapper::toDto)
+		return repository.findById(id).map(AddressMapper::toDto)
 				.orElseThrow(() -> new ModelNotFound("Address", id));
 	}
-	
+
 	List<AddressDto> getAddresses() {
 		return StreamSupport.stream(repository.findAll().spliterator(), false)
 				.map(AddressMapper::toDto)
 				.collect(Collectors.toList());
 	}
-	
-	
+
 	void deleteAddress(long id) {
-		Address address = repository.findById(id)
-				.orElseThrow(() -> new ModelNotFound("Address", id));
+		Address address = repository.findById(id).orElseThrow(() -> new ModelNotFound("Address", id));
 		repository.delete(address);
 	}
-	
-	
-    void update(AddressDto addressDto){
-        Address address = repository.findById(addressDto.getId())
-                .orElseThrow(() -> new ModelNotFound("Address", addressDto.getId()));
-        EntityUtils.setter(addressDto.getStreet(), t -> address.setStreet(t));
-        EntityUtils.setter(addressDto.getStreetNumber(), t -> address.setStreetNumber(t));
-        EntityUtils.setter(addressDto.getAdditionalNumber(), t -> address.setAdditionalNumber(t));
-        EntityUtils.setter(addressDto.getPostalCode(), t -> address.setPostalCode(t));
-        EntityUtils.setter(addressDto.getClient(), t -> address.setClient(t));
-        EntityUtils.setter(addressDto.getCity(), t -> address.setCity(t));
-        repository.save(address);
-    }
 
-	
-	List<AddressDto> getAddressesSearch(String street, String streetNumber,
-			String additionalNumber, String postalCode, String city, Client client) {
+	void update(AddressDto addressDto) {
+		Address address = repository.findById(addressDto.getId())
+				.orElseThrow(() -> new ModelNotFound("Address", addressDto.getId()));
+		EntityUtils.setter(addressDto.getStreet(), t -> address.setStreet(t));
+		EntityUtils.setter(addressDto.getStreetNumber(), t -> address.setStreetNumber(t));
+		EntityUtils.setter(addressDto.getAdditionalNumber(), t -> address.setAdditionalNumber(t));
+		EntityUtils.setter(addressDto.getPostalCode(), t -> address.setPostalCode(t));
+		EntityUtils.setter(addressDto.getClient(), t -> address.setClient(t));
+		EntityUtils.setter(addressDto.getCity(), t -> address.setCity(t));
+		repository.save(address);
+	}
+
+	List<AddressDto> getAddressesSearch(String street, String streetNumber, String additionalNumber, String postalCode,
+			String city, Client client) {
 		List<AddressDto> dtos = StreamSupport.stream(repository.findAll().spliterator(), false)
-				.map(AddressMapper::toDto)
-				.collect(Collectors.toList());
+				.map(AddressMapper::toDto).collect(Collectors.toList());
 		return null;
 	}
-	
+
 	private static List<AddressDto> filter(List<AddressDto> dtos, String street, String streetNumber,
-			String additionalNumber, String postalCode, String city, Client client){
-		return dtos.stream()
-				.filter(addFilter(street, addressDto -> street.equalsIgnoreCase(addressDto.getStreet())))
-				.filter(addFilter(streetNumber, addressDto -> streetNumber.equalsIgnoreCase(addressDto.getStreetNumber())))
-				.filter(addFilter(additionalNumber, addressDto -> additionalNumber.equalsIgnoreCase(addressDto.getAdditionalNumber())))
+			String additionalNumber, String postalCode, String city, Client client) {
+		return dtos.stream().filter(addFilter(street, addressDto -> street.equalsIgnoreCase(addressDto.getStreet())))
+				.filter(addFilter(streetNumber,
+						addressDto -> streetNumber.equalsIgnoreCase(addressDto.getStreetNumber())))
+				.filter(addFilter(additionalNumber,
+						addressDto -> additionalNumber.equalsIgnoreCase(addressDto.getAdditionalNumber())))
 				.filter(addFilter(postalCode, addressDto -> postalCode.equalsIgnoreCase(addressDto.getPostalCode())))
 				.filter(addFilter(city, addressDto -> city.equalsIgnoreCase(addressDto.getCity())))
 				.filter(addFilter(client, addressDto -> client.equals(addressDto.getClient())))
 				.collect(Collectors.toList());
 	}
-	
-    private static Predicate<AddressDto> addFilter(Object value, Predicate<AddressDto> predicate) {
-        if(value != null){
-            return predicate;
-        }
-        return AddressDto -> true;
-    }
-	
-	
+
+	private static Predicate<AddressDto> addFilter(Object value, Predicate<AddressDto> predicate) {
+		if (value != null) {
+			return predicate;
+		}
+		return AddressDto -> true;
+	}
+
 }
